cd49c86a89371fca8112067810040f3902c7d4bf
Rubén Conde Magaña <code@rubn.dev>
2022-07-29 14:23:49 +0200

    refactor: :recycle: CommonJS
    Signed-off-by: Rubén Conde Magaña <code@rubn.dev>

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-import { getInput, setFailed, setOutput } from "@actions/core";
-import { context } from "@actions/github";
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const withOwner = getInput("with-owner");
+  const withOwner = core.getInput("with-owner");
   let name = "";
 
   switch (withOwner) {
     case "false":
-      name = context.repo.repo.split("/")[0];
+      name = github.context.repo.repo.split("/")[0];
       break;
     case "true":
-      name = context.repo.repo;
+      name = github.context.repo.repo;
       break;
     default:
-      name = context.repo.repo;
+      name = github.context.repo.repo;
       break;
   }
 
-  setOutput("repository-name", name);
+  core.setOutput("repository-name", name);
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
-  setFailed(error.message);
+  core.setFailed(error.message);
 }
