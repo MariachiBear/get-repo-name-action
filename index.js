08cbc9b4a75e187e8456ff9647d43f5ceb7b250b
Rubén Conde Magaña <code@rubn.dev>
2022-07-29 16:41:26 +0200

    feat: :sparkles: Use actions exec
    Signed-off-by: Rubén Conde Magaña <code@rubn.dev>

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-import { getInput, setFailed, setOutput } from "@actions/core";
-import { context } from "@actions/github";
+const core = require("@actions/core");
+const exec = require("@actions/exec");
 
 try {
   // `who-to-greet` input defined in action metadata file
   const withOwner = getInput("with-owner");
   let name = "";
+  let myOutput = "";
+  let myError = "";
+
+  const options = {};
+  options.listeners = {
+    stdout: (data) => {
+      myOutput += data.toString();
+    },
+    stderr: (data) => {
+      myError += data.toString();
+    },
+  };
+
+  await exec.exec(
+    "git config --get remote.origin.url | sed -e 's/^git@.*:([[:graph:]]*).git/\1/'",
+    [],
+    options
+  );
 
   switch (withOwner) {
     case "false":
-      name = context.repo.repo.split("/")[0];
+      name = myOutput.split("/").reverse()[0];
       break;
     case "true":
-      name = context.repo.repo;
+      name = myOutput;
       break;
     default:
-      name = context.repo.repo;
+      name = myOutput;
       break;
   }
 
