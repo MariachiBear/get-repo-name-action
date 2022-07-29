77211bdef074d80a4b50c45d3c6ce87ee0262c09
Rubén Conde Magaña <code@rubn.dev>
2022-07-29 17:39:32 +0200

    feat: :sparkles: Code formatting
    Signed-off-by: Rubén Conde Magaña <code@rubn.dev>

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const core = require("@actions/core");
-const exec = require("@actions/exec");
+/* eslint-disable no-undef */
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const github = require('@actions/github');
 
-function getRepositoryName() {
-  let myOutput = "";
-  let myError = "";
+async function getRepositoryName() {
+  let myOutput = '';
+  let myError = '';
 
   const options = {};
   options.listeners = {
@@ -15,55 +17,50 @@ function getRepositoryName() {
     },
   };
 
-  await exec.exec(
-    "git config --get remote.origin.url | sed -e 's/^git@.*:([[:graph:]]*).git/\1/'",
-    [],
-    options
-  );
+  await exec.exec('git config --get remote.origin.url', [], options);
 
-  return myOutput;
+  const repository = myOutput.match('(?:git@|https://)github.com[:/](.*).git');
+
+  return repository[2];
 }
 
 try {
-  let name = "";
+  let name = '';
 
-  const repName = getRepositoryName()
-  const withOwner = core.getInput("with-owner");
+  getRepositoryName().then((repName) => {
+    const withOwner = core.getInput('with-owner');
+    const [owner, repo] = repName.split('/');
 
-  switch (withOwner) {
-    case "false":
-      name = repName.split("/").reverse()[0];
-      break;
-    case "true":
-      name = repName;
-      break;
-    default:
-      name = repName;
-      break;
-  }
+    switch (withOwner) {
+      case 'false':
+        name = repo;
+        break;
+      default:
+        name = `${owner}/${repo}`;
+        break;
+    }
 
-  let casedName = ''
-  const stringCase = core.getInput("string-case");
+    let casedName = '';
+    const stringCase = core.getInput('string-case');
 
-  switch (stringCase) {
-    case "lowercase":
-      casedName = name.toLowerCase();
-      break;
-    case "uppercase":
-      casedName = name.toUpperCase();
-      break;
-    default:
-      casedName = name;
-      break;
-  }
+    switch (stringCase) {
+      case 'lowercase':
+        casedName = name.toLowerCase();
+        break;
+      case 'uppercase':
+        casedName = name.toUpperCase();
+        break;
+      default:
+        casedName = name;
+        break;
+    }
 
-  core.setOutput("repository-name", casedName);
+    core.setOutput('repository-name', casedName);
+  });
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2);
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
-
-
